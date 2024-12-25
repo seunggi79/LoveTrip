e8c85c72a679af3a91e821e1778c3697ec3f005e
seunggi <hsg77689469.gamil.com>
2024-12-25 11:41:26 +0900

    hotel type

--- src/remote/hotel.ts
+++ src/remote/hotel.ts
@@ -1,4 +1,5 @@
 import { COLLECTION } from '@/contants'
+import { Hotel } from '@/models/hotel'
 import {
   collection,
   getDocs,
@@ -9,7 +10,7 @@ import {
 } from 'firebase/firestore'
 import { store } from './firebase'
 
-export async function getHotels(pageParams?: QuerySnapshot<unknown>) {
+export async function getHotels(pageParams?: QuerySnapshot<Hotel>) {
   const hotelsQuery =
     pageParams == null
       ? query(collection(store, COLLECTION.HOTEL), limit(10))
@@ -19,10 +20,13 @@ export async function getHotels(pageParams?: QuerySnapshot<unknown>) {
           limit(10),
         )
   const hotelsSnapshot = await getDocs(hotelsQuery)
-  const items = hotelsSnapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }))
+  const items = hotelsSnapshot.docs.map(
+    (doc) =>
+      ({
+        id: doc.id,
+        ...doc.data(),
+      }) as Hotel,
+  )
   const lastVisible = hotelsSnapshot.docs[hotelsSnapshot.docs.length - 1]
 
   return {
